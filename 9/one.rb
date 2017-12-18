24264315de46560256dde12fdfe0f3dfb9e0228c
Greg Woodcock <pr1sm.g@gmail.com>
2017-12-18 22:28:19 +0000

    Day 9 part 2

--- 9/one.rb
+++ 9/one.rb
@@ -5,18 +5,20 @@ nesting = 0
 i = 0
 score = 0
 inside_garbage = false
+garbage = 0
 
 while i < input.length do
   x = input[i]
   
      if  x == '!' then i += 1
-  elsif  x == '<' then inside_garbage = true
+  elsif  x == '<' && !inside_garbage then inside_garbage = true
   elsif  x == '>' then inside_garbage = false
-  elsif inside_garbage then nil # noop
+  elsif inside_garbage then garbage += 1
   elsif  x == '{' then score += (nesting += 1)
   elsif  x == '}' then nesting -= 1 end
   i += 1
 end
 
 
-p score
+p "Score: #{score}"
+p "Garbage: #{garbage}"
