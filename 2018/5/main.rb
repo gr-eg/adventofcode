513333b7d4c6608bfc6eea9ed0eea682be34dd74
Greg Woodcock <greg.woodcock@raspberrypi.org>
2018-12-05 21:19:00 +0000

    Code golf

--- 2018/5/main.rb
+++ 2018/5/main.rb
@@ -2,17 +2,14 @@
 
 def react(input)
   new_input = input.sub(/((.)(?-i)(?!\2)(?i)\2)/, '')
-  return new_input if input == new_input
-  react(new_input)
+  input == new_input ? new_input : react(new_input)
 end
 
 input = File.read('input.txt').chomp
 puts "Part 1: #{react(input).chars.count}"
 
-polymers = []
-
-input.chars.map(&:downcase).uniq.map do |char|
-  polymers << react(input.gsub(/#{char}/i, '')).chars.count
+polymers = input.chars.map(&:downcase).uniq.map do |char|
+  react(input.gsub(/#{char}/i, '')).chars.count
 end
 
 puts "Part 2: #{polymers.min}"
