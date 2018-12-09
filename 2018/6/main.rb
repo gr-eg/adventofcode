306355a7498f36e655ad340d3482faedd7fd5f11
Greg Woodcock <greg.woodcock@raspberrypi.org>
2018-12-09 16:58:43 +0000

    Day 6  - Part 2

--- 2018/6/main.rb
+++ 2018/6/main.rb
@@ -6,6 +6,7 @@ input = File.readlines('input.txt').map { |x| x.chomp.split(', ').map(&:to_i) }
 arr = Array.new(0, [])
 
 edges = []
+distances = Hash.new(0)
 ymax = (input.max_by { |x| x[0] }[0]) + 1
 xmax = input.max_by { |x| x[1] }[1]
 
@@ -15,15 +16,16 @@ xmax = input.max_by { |x| x[1] }[1]
 
     input.each do |place|
       distance = (y - place[0]).abs + (x - place[1]).abs
-      letter = input.index(place)
+      name = input.index(place)
+      distances["#{x},#{y}"] += distance
 
       if arr[x][y] && arr[x][y][1].to_i == distance
-        arr[x][y] = ['.', distance]
+          arr[x][y] = ['.', distance]
       else
         unless arr[x][y] && arr[x][y][1].to_i < distance
-          arr[x][y] = [letter, distance]
+          arr[x][y] = [name, distance]
           if x.zero? || y.zero? || x == xmax + 1 || y == ymax
-            edges << [letter]
+            edges << [name]
           end
         end
       end
@@ -34,4 +36,6 @@ end
 finite = (0...input.count).to_a - edges.uniq.flatten
 part1 = arr.map { |x| x.map(&:first) }.flatten.keep_if { |x| finite.include? x }.group_by(&:itself).map { |k,v| [k, v.count] }.max_by { |x| x[1]}[1]
 puts "Part 1: #{part1}"
-# puts arr.map { |a| a.map { |i| i[0].to_s.rjust(7) }.join }
+
+part2 = distances.values.select { |x| x < 10_000 }.count
+puts "Part 2: #{part2}"
