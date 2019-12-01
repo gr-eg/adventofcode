4ae9e5d2347fcecedee3e0d4b22b4db53ccd2656
Greg Woodcock <pr1sm.g@gmail.com>
2019-12-01 17:59:13 +0000

    Day one - Part 2

--- 2019/1/main.rb
+++ 2019/1/main.rb
@@ -1,5 +1,14 @@
-lines = File.readlines('input.txt')
+require 'benchmark'
+lines = File.readlines('input.txt').map(&:to_i)
 
-puts lines.map { |x| (x.to_i / 3).floor - 2 }.sum
+def calc_required(amount)
+  (amount / 3).floor - 2
+end
 
+def calc_fuel(amount)
+  required_next = calc_required(amount)
+  required_next > 0 ? (required_next + calc_fuel(required_next)) : 0
+end
 
+puts "Part 1: #{lines.map { |x| calc_required(x) }.sum}"
+puts "Part 2: #{lines.map { |x| calc_fuel(x) }.sum}"
