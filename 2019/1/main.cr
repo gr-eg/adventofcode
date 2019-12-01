e6d10334362dd4ae161a1b8d7fe7a1b621fe2f81
Greg Woodcock <pr1sm.g@gmail.com>
2019-12-01 18:13:39 +0000

    Implementation in crystal

---
+++ 2019/1/main.cr
@@ -0,0 +1,13 @@
+lines = File.read_lines("input.txt").map { |i| i.to_i }
+
+def calc_required(amount)
+  (amount / 3).floor - 2
+end
+
+def calc_fuel(amount)
+  required_next = calc_required(amount)
+  required_next > 0 ? (required_next + calc_fuel(required_next)) : 0
+end
+
+puts "Part 1: #{lines.map { |x| calc_required(x) }.sum }"
+puts "Part 2: #{lines.map { |x| calc_fuel(x) }.sum }"
